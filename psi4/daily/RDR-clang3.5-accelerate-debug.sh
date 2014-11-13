5a42d5fcbc9ff64521baff0ff10cc413915e82bc
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-11-13 12:09:46 +0100

    Add a script for merzbow

---
+++ psi4/daily/RDR-clang3.5-accelerate-debug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PSI4_TMPDIR=/Users/roberto/Scratch/RDR-clang3.5-accelerate-debug
+mkdir -p $PSI4_TMPDIR
+export PSI4_TMPDIR
+export CTEST_MAKE_NUM_PROCS=6
+
+TMP_DIR=/Users/roberto/Scratch/tmprunpsi4/RDR-clang3.5-accelerate-debug
+mkdir -p $TMP_DIR
+
+git clone git@github.com:robertodr/psi4.git $TMP_DIR
+
+cd $TMP_DIR
+
+./setup --fc=gfortran --cc=clang --cxx=clang++ --type=debug --plugins=on --accelerate -D BUILDNAME=RDR-clang3.5-accelerate-debug -D SITE=merzbow
+
+cd $TMP_DIR/obj
+
+export PSI4DATADIR=$TMP_DIR/lib
+mkdir -p $PSI4_TMPDIR/psi4scr
+export PSI4_SCRATCH=$PSI4_TMPDIR/psi4scr
+
+ctest -D Nightly -j6
+
+cd
+rm -rf $PSI4_TMPDIR $TMP_DIR
+
+exit 0
