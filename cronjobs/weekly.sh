ad8c66d425b978522b714a85de2cd04c4d1822e1
Roberto Di Remigio <roberto.d.remigio@uit.no>
2014-11-16 23:02:41 +0100

    Add valgrind and sanitizer weekly jobs

--- cronjobs/weekly.sh
+++ cronjobs/weekly.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
+# Written by Arnfinn Hykkerud Steindal
+# adapted by Roberto Di Remigio
 
 export PATH=$PATH:/opt/torque/bin
 
-cd /home/arnfinn/cronjobs/
+cd /home/roberto/Workspace/cdash-recipes/psi4/weekly
+rm *.o*
+rm *.e*
 
-rm *.pbs.*
+DAY=$(date +%a)
 
-for i in 2013-*.pbs; do
+for i in RDR-*.pbs; do
     qsub $i
 done
-
-#for i in long-*.pbs; do
-#    qsub $i
-#done
-
-for i in oRSP*release.pbs; do
-    qsub $i
-done
-
-#for i in oRSP-*debug.pbs; do
-#    qsub $i
-#done
